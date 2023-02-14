df1dacebfe39417fc7b2d15f756411c07799c089
shubham sharma <shubham00sharma0@gmail.com>
2023-02-15 00:44:30 +0530

    initial

---
+++ src/test/java/com/reportcard/reportcard/ReportcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reportcard.reportcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
